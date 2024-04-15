f22f9ff5bedb59b9912dde85a5317282f1dca61b
ktolleshaug <ktolleshaug@gmail.com>
2024-04-14 20:09:31 -0500

    init, and refactor from old repo

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import video from "./NeoIsTheOne.mp4" 
+function App() {
+  return (
+    <div className="App absolute text-white w-screen h-screen">
+      <div className="flex flex-col h-full w-full items-center justify-center">
+        <video className="w-11/12" controls>
+          <source src={video} type="video/mp4"></source>
+        </video>
+        <span className=" text-sm">The Matrix&copy; is owned by Warner Bros. Pictures.</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
